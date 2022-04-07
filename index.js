2f8214df23c40a0752f9810aa9f7e342340b2eb6
Martin <mkmartinoes@gmail.com>
2022-04-07 16:20:26 +0300

    test: distance from HQ in feets

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
  const distanceFromHqInBlocks = function(distance){
     return Math.abs(distance - 42);
  }
+
+ const distanceFromHqInFeet = function(feet){
+     let distanceInFeet = (Math.abs(feet -42) * 264);
+     return distanceInFeet;
+ }
